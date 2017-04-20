9317ba3df18a2786a703ffc04e7a542ea3a15d35
iddiaz <diazdiazivan@gmail.com>
2017-04-21 00:36:44 +0200

    Configuración de metodo GET para devolver datos desde Api rest

--- index.js
+++ index.js
@@ -21,11 +21,26 @@ app.use(bodyParser.json())
 
 // obtener productos
 app.get('/api/product', (req,res) =>{
-  res.status(200).send({products: []});
+  // al estar vacio busca todos los productos
+  Product.find({}, (err, products)=>{
+    if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`});
+
+    if(!products) return res.status(404).send({message: 'No existen productos'});
+
+    res.status(200).send({ products: products });
+  });
+
 });
 
 app.get('/api/product/:productId', (req, res) => {
+  let productId = req.params.productId;
 
+  Product.findById(productId, (err, product) => {
+    if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`});
+    if(!product) return res.status(404).send({message: 'El producto no existe'});
+
+    res.status(200).send({product: product});
+  });
 });
 
 // subir productos
